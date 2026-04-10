2f32582a18b8b4a4c045b5760eede418a7cdd3ea
Manasvi05Dadhich <manasvidadhich05@gmail.com>
2026-04-10 22:20:03 +0530

    feat : added home page functionality + uuid

--- collab.code/src/App.js
+++ collab.code/src/App.js
@@ -2,10 +2,25 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import CodeEditor from './pages/codeEditor';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <>
+      <div>
+        <Toaster
+          position="top-center"
+          toastOptions={{
+            className: '',
+            duration: 5000,
+            removeDelay: 1000,
+            style: {
+              background: '#363636',
+              color: '#fff',
+            },
+          }}
+        />
+      </div>
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home />} />
--- collab.code/src/pages/Home.js
+++ collab.code/src/pages/Home.js
@@ -1,24 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+import toast, { Toaster } from 'react-hot-toast';
 
 function Home() {
+
+    const [newId, setnewID] = useState('');
+    const [username, setUsername] = useState('');
+    const createNewRoom = (e) => {
+        e.preventDefault();
+        const id = uuidv4();
+        setnewID(id);
+        // console.log(id);
+        toast.success('Created new room especially for you')
+    }
     return <div
         className="HomePageWrapper">
         <div className="FormWrapper">
-
             <img src='/logo.svg' alt="loogoooooo"></img>
-            <h4> Paste Room Invitation ID</h4>
-            <div className="form">
-                <input type="text" className="roomId" placeholder="ROOM ID"></input>
-                <input type="text" className="userName" placeholder="USERNAME"></input>
+            <h4 className="label"> Paste Room Invitation ID</h4>
+            <div className="Form">
+                <input type="text"
+                    className="roomId"
+                    placeholder="ROOM ID"
+                    onChange={(e) => setnewID(e.target.value)}
+                    value={newId}>
+                </input>
+                <input type="text"
+                    className="userName"
+                    placeholder="USERNAME"
+                    onChange={(e) => setUsername(e.target.value)}
+                    value={username}></input>
                 <button className="joinBtn"> Enter</button>
             </div>
             <span className="moreInfo">
                 Create RoomId Here &nbsp;
-                <a href="" className="createRoom"> New Room </a>
+                <a href="" className="createRoom" onClick={createNewRoom}> New Room </a>
             </span>
         </div>
         <footer>
-            <h4>Built with love by {''}
+            <h4>Built with ♡ by {''}
                 <a href="https://github.com/Manasvi05Dadhich">
                     Manasvi
                 </a>
